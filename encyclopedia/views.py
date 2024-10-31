21790986ab0065af3df877215af1686a471893e9
Vivek <vivdave2004@gmail.com>
2024-10-31 15:23:36 +0530

    created initial redirects for entries and create page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,4 +7,10 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+def page(request, title):
+    if title in util.list_entries():
+        return render(request, "encyclopedia/wikitemplate.html")
+    elif title not in util.list_entries():
+        return render(request, "encyclopedia/404page.html")
+def create(request):
+    return render(request, "encyclopedia/create.html")
